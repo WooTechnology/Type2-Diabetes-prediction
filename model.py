56e14fddffb2dcb16567fe44f128421512e6074b
Anushka Narula <anushkanarula01@gmail.com>
2020-01-18 00:21:41 +0530

    Added file

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[24]:
+# In[4]:
 
 
 import pandas as pd
@@ -11,13 +11,13 @@ import matplotlib.pyplot as plt
 get_ipython().run_line_magic('matplotlib', 'inline')
 
 
-# In[2]:
+# In[5]:
 
 
 pwd
 
 
-# In[3]:
+# In[10]:
 
 
 variable=pd.read_csv(r"C:\Users\aknar\Type2-Diabetes-prediction\Dataset\diabetes.csv")
@@ -31,7 +31,7 @@ variable
 variable.describe()
 
 
-# In[6]:
+# In[11]:
 
 
 #information of dataset
@@ -86,7 +86,7 @@ sns.set(style="whitegrid")
 variable.boxplot(figsize=(15,6))
 
 
-# In[15]:
+# In[12]:
 
 
 #box plot
@@ -100,7 +100,7 @@ sns.boxplot(x=variable['DIABETES PEDIGREE FUNCTION'])
 plt.show()
 
 
-# In[18]:
+# In[14]:
 
 
 #outlier remove
@@ -114,7 +114,7 @@ print("\n---IQR---\n",IQR)
 #print((df < (Q1 - 1.5 * IQR))|(df > (Q3 + 1.5 * IQR)))
 
 
-# In[19]:
+# In[15]:
 
 
 #outlier remove
@@ -123,7 +123,7 @@ variable.shape,variable_out.shape
 #more than 80 records deleted
 
 
-# In[20]:
+# In[16]:
 
 
 #Scatter matrix after removing outlier
@@ -132,7 +132,7 @@ sns.pairplot(variable_out, hue="OUTCOME")
 plt.show()
 
 
-# In[21]:
+# In[17]:
 
 
 #lets extract features and targets
@@ -140,7 +140,7 @@ X=variable_out.drop(columns=['OUTCOME'])
 y=variable_out['OUTCOME']
 
 
-# In[22]:
+# In[18]:
 
 
 #Splitting train test data 80 20 ratio
@@ -148,30 +148,119 @@ from sklearn.model_selection import train_test_split
 train_X,test_X,train_y,test_y=train_test_split(X,y,test_size=0.2)
 
 
-# In[23]:
+# In[19]:
 
 
 train_X.shape,test_X.shape,train_y.shape,test_y.shape
 
 
-# In[24]:
-# Applying logistic regression model
+# In[20]:
+
+
+from sklearn.metrics import confusion_matrix,accuracy_score,make_scorer
+from sklearn.model_selection import cross_validate
+
+def tn(y_true, y_pred): return confusion_matrix(y_true, y_pred)[0, 0]
+def fp(y_true, y_pred): return confusion_matrix(y_true, y_pred)[0, 1]
+def fn(y_true, y_pred): return confusion_matrix(y_true, y_pred)[1, 0]
+def tp(y_true, y_pred): return confusion_matrix(y_true, y_pred)[1, 1]
+
+#cross validation purpose
+scoring = {'accuracy': make_scorer(accuracy_score),'prec': 'precision'}
+scoring = {'tp': make_scorer(tp), 'tn': make_scorer(tn),
+           'fp': make_scorer(fp), 'fn': make_scorer(fn)}
+def display_result(result):
+    print("TP: ",result['test_tp'])
+    print("TN: ",result['test_tn'])
+    print("FN: ",result['test_fn'])
+    print("FP: ",result['test_fp'])
+
+
+# In[21]:
+
+
+#Logistic Regression
 from sklearn.linear_model import LogisticRegression
-
 from sklearn.metrics import roc_auc_score
-def find_model_perf(X_train, y_train, X_test, y_test):
-    model = LogisticRegression()
-    model.fit(train_X, train_y)
-    y_hat = [x[1] for x in model.predict_proba(test_X)]
-    auc = roc_auc_score(y_test, y_hat)
-    score = model.score(test_X, test_y)
-    
-    print("AUC:",auc)
-    print("Accuracy Score: ",score)
-    
-auc_processed = find_model_perf(train_X, train_y, test_X, test_y)
-print(auc_processed)
 
-# Result is  as follows:
-#AUC: 0.8014950166112957
-#Accuracy Score:  0.7421875
+acc=[]
+roc=[]
+
+clf=LogisticRegression()
+clf.fit(train_X,train_y)
+y_pred=clf.predict(test_X)
+#find accuracy
+ac=accuracy_score(test_y,y_pred)
+acc.append(ac)
+
+#find the ROC_AOC curve
+rc=roc_auc_score(test_y,y_pred)
+roc.append(rc)
+print("\nAccuracy {0} ROC {1}".format(ac,rc))
+
+#cross val score
+result=cross_validate(clf,train_X,train_y,scoring=scoring,cv=10)
+display_result(result)
+
+#display predicted values uncomment below line
+#pd.DataFrame(data={'Actual':test_y,'Predicted':y_pred}).head()
+
+
+# In[23]:
+
+
+#Support Vector Machine
+from sklearn.svm import SVC
+
+clf=SVC(kernel='linear')
+clf.fit(train_X,train_y)
+y_pred=clf.predict(test_X)
+#find accuracy
+ac=accuracy_score(test_y,y_pred)
+acc.append(ac)
+
+#find the ROC_AOC curve
+rc=roc_auc_score(test_y,y_pred)
+roc.append(rc)
+print("\nAccuracy {0} ROC {1}".format(ac,rc))
+
+#cross val score
+result=cross_validate(clf,train_X,train_y,scoring=scoring,cv=10)
+display_result(result)
+
+#display predicted values uncomment below line
+#pd.DataFrame(data={'Actual':test_y,'Predicted':y_pred}).head()
+
+
+# In[24]:
+
+
+#KNN
+
+from sklearn.neighbors import KNeighborsClassifier
+
+clf=KNeighborsClassifier(n_neighbors=3)
+clf.fit(train_X,train_y)
+y_pred=clf.predict(test_X)
+#find accuracy
+ac=accuracy_score(test_y,y_pred)
+acc.append(ac)
+
+#find the ROC_AOC curve
+rc=roc_auc_score(test_y,y_pred)
+roc.append(rc)
+print("\nAccuracy {0} ROC {1}".format(ac,rc))
+
+#cross val score
+result=cross_validate(clf,train_X,train_y,scoring=scoring,cv=10)
+display_result(result)
+
+#display predicted values uncomment below line
+#pd.DataFrame(data={'Actual':test_y,'Predicted':y_pred}).head()
+
+
+# In[ ]:
+
+
+
+
